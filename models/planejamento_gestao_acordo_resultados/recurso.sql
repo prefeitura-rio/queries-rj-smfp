9a05cecb768014c06b4de0d8a5c589b5c51d0186
Bruno Almeida <42002476+BrunodePauloAlmeida@users.noreply.github.com>
2024-10-16 16:03:55 -0300

    fix: column type

--- models/planejamento_gestao_acordo_resultados/recurso.sql
+++ models/planejamento_gestao_acordo_resultados/recurso.sql
@@ -1,8 +1,8 @@
 SELECT
-    SAFE_CAST(cod_recurso AS STRING) as id_recurso,
+    SAFE_CAST(SAFE_CAST(SAFE_CAST(cod_recurso AS FLOAT64) AS INT64) AS STRING) as id_recurso,
     SAFE_CAST(ano_do_acordo_a_que_se_refere_o_recurso AS INT64) as ano,
     SAFE_CAST(orgao_solicitante AS STRING) as orgao,
-    SAFE_CAST(cod_meta AS STRING) as codigo_meta,
+    SAFE_CAST(SAFE_CAST(SAFE_CAST(cod_meta AS FLOAT64) AS INT64) AS STRING) as codigo_meta,
     SAFE_CAST(orgao_acordo_egpweb AS STRING) as orgao_egpweb,    
     SAFE_CAST(oficio AS STRING) as oficio,
     SAFE_CAST(codigo_processorio AS STRING) as codigo_processorio,
@@ -13,4 +13,4 @@ SELECT
     SAFE_CAST(avaliacao_egp AS BOOL) as avaliacao_egp,
     SAFE_CAST(SAFE_CAST(tipo_do_recurso AS FLOAT64) AS INT64) as tipo_recurso,
     SAFE_CAST(decisao_do_prefeito AS BOOL) as indicador_decisao_recurso
-FROM `rj-smfp.planejamento_gestao_acordo_resultados_staging.recurso`
+FROM rj-smfp.planejamento_gestao_acordo_resultados_staging.recurso
